c529fa1eec6a51bbddaec42c025ca7c49de801d4
Agumas-Alehegn <156206504+Agumas-Alehegn@users.noreply.github.com>
2024-10-10 04:29:01 +0300

    integrate loader functionality while fetching data

--- amazon/src/component/Pages/ProductDetail/ProductDetail.jsx
+++ amazon/src/component/Pages/ProductDetail/ProductDetail.jsx
@@ -5,42 +5,51 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { productUrl } from "../../Api/endPoints";
 import ProductsCard from "../../Product/ProductsCard.jsx";
+import Loading from "../../Loader/Loading.jsx";
 
 function ProductDetail() {
   const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const { productId } = useParams();
   console.log(productId);
   // This will print the productId received from the URL;
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`${productUrl}/products/${productId}`)
       .then((res) => {
         console.log(res.data);
         setProduct(res.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setIsLoading(false);
       });
   }, []);
   console.log(product);
   return (
     <LayOut>
-      <div className={classes.details_wrap}>
-        <ProductsCard product={product} />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className={classes.details_wrap}>
+          <ProductsCard product={product} />
 
-        <div style={{ marginBottom: "50px " }}>
-          <h2>Product Details</h2>
-          <h3>{product.title}</h3>
-          <p>
-            <strong>Category: </strong>
-            {product.category}
-          </p>
-          <p>
-            <strong>Description: </strong>
-            {product.description}
-          </p>
+          <div style={{ marginBottom: "50px " }}>
+            <h2>Product Details</h2>
+            <h3>{product.title}</h3>
+            <p>
+              <strong>Category: </strong>
+              {product.category}
+            </p>
+            <p>
+              <strong>Description: </strong>
+              {product.description}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </LayOut>
   );
 }
